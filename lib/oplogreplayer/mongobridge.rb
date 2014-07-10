7ad76f92d874b618e0ba43ca276b562303a564d7
brettcave <brett@cave.za.net>
2014-07-10 13:13:20 +0200

    PYKE-144 / PYKE-668 - replay operations.

--- lib/oplogreplayer/mongobridge.rb
+++ lib/oplogreplayer/mongobridge.rb
@@ -7,6 +7,8 @@ module Oplogreplayer
 
     @_mongoClient
     @log
+    @filterDbs = nil
+
 
     def initialize(config)
 
@@ -18,7 +20,7 @@ module Oplogreplayer
       connect_uri += "#{config["username"]}:#{config["password"]}"
       connect_uri += "@#{config["host"]}"
       connect_uri += "/#{config["initialDb"]}"
-      connect_uri += "?replicaSet=#{sourceConfig["replicaSet"]}" unless config["mode"] == "single"
+      connect_uri += "?replicaSet=#{config["replicaSet"]}" unless config["mode"] == "single"
 
       if config["mode"] == "single"
         @log.info("Setting up single target")
@@ -27,13 +29,19 @@ module Oplogreplayer
         @log.info("Setting up RS target")
         @_mongoClient = Mongo::MongoReplicaSetClient.from_uri(connect_uri)
       end
+
+      if config["onlyDbs"]
+        @filterDbs = config["onlyDbs"].split(",")
+        @log.info("Filter Dbs has been configured: #{@filterDbs.to_s}")
+      end
     end
 
     def getOplogTimestamp()
       findStamp = @_mongoClient.db("local").collection("oplog.tracker").find_one()
       if findStamp
         @log.info("Stamp found: #{findStamp.inspect}")
-        findStamp["timestamp"]
+        # We return timestamp+1 as "timestamp" was already replayed before shutdown.
+        findStamp["timestamp"]+1
       else
         @log.info("No stamp found")
         nil
@@ -48,40 +56,69 @@ module Oplogreplayer
     end
 
     def insert(db_name, collection_name, document)
-      if (db_name == "foo")
-        @log.info("Found an insert: #{db_name}.#{collection_name} => #{document.inspect}")
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("insert #{db_name}.#{collection_name} : #{document.inspect}")
+        @_mongoClient.db(db_name).collection(collection_name).insert(document)
       end
     end
-    def remove(db_name,collection_name, document)
 
+    def remove(db_name,collection_name, document)
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("remove #{db_name}.#{collection_name} : #{document.inspect}")
+        @_mongoClient.db(db_name).collection(collection_name).remove(document)
+      end
     end
+
     def update(db_name, collection_name,selector,update)
-      if (db_name == "foo")
-        @log.info("Found an update: #{db_name}.#{collection_name} => #{selector} => #{update}")
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("update #{db_name}.#{collection_name} : #{selector} : #{update}")
+        @_mongoClient.db(db_name).collection(collection_name).update(selector,update)
       end
     end
 
 
     def create_index(db_name, collection_name, index_key, options)
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("create_index #{db_name}.#{collection_name} : #{index_key} : #{options}")
+        @_mongoClient.db(db_name).collection(collection_name).create_index(index_key,options)
+      end
     end
-    def drop_index(db_name, collection_name, index_name)
 
+    def drop_index(db_name, collection_name, index_name)
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("drop_index #{db_name}.#{collection_name} : #{index_name}")
+        @_mongoClient.db(db_name).collection(collection_name).drop_index(index_name)
+      end
     end
 
     def create_collection(db_name, collection_name,  options)
-
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("create_collection #{db_name} : #{collection_name} : #{options}")
+        @_mongoClient.db(db_name).create_collection(collection_name,options)
+      end
     end
     def drop_collection(db_name, collection_name)
-
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("drop_collection #{db_name} : #{collection_name}")
+        @_mongoClient.db(db_name).drop_collection(collection_name)
+      end
     end
 
     def rename_collection(db_name, old_collection_name, new_collection_name)
-
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("rename_collection #{db_name} : from #{old_collection_name} to #{new_collection_name}")
+        @_mongoClient.db(db_name).rename_collection(old_collection_name,new_collection_name)
+      end
     end
 
     def drop_database(db_name)
-
+      if @filterDbs and @filterDbs.include? db_name
+        @log.debug("drop_database #{db_name}")
+        @_mongoClient.drop_database(db_name)
+      end
     end
+
+    private
 
     def setupLogger()
       @log = Log4r::Logger.new("Oplogreplay::Mongobridge")
